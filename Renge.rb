1e7031a196ee8404342efc99545cdb81acd7b801
Masaya Takeda <844196@gmail.com>
2015-01-16 06:04:14 +0900

    ver 0.2

--- Renge.rb
+++ Renge.rb
@@ -2,13 +2,13 @@ require "formula"
 
 class Renge < Formula
     homepage "https://github.com/844196/Renge"
-    url "https://github.com/844196/Renge/archive/v0.1.tar.gz"
-    sha256 "64438e59f82f9759ee41619cb8b8294732c37ba1a24e8fd52f5aa4a2a21ad15f"
+    url "https://github.com/844196/Renge/archive/v0.2.tar.gz"
+    sha256 "baef2c7074d9c7649fef5dc56e32a66be9b78a313ced4fa27ffaf76813f59f1f"
     head "https://github.com/844196/Renge.git"
-    version "0.1"
+    version "0.2"
 
     def install
-        bin.install "fortunerenge"
+        bin.install "renge"
         prefix.install "renge-quotes"
         bin.install_symlink "#{prefix}/renge-quotes" => "renge-quotes"
     end
