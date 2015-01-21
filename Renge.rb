749b68d12c1454eab7b706fcac42df36935b2af4
Masaya Takeda <844196@gmail.com>
2015-01-21 19:15:00 +0900

    ver 0.6

--- Renge.rb
+++ Renge.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Renge < Formula
     homepage "https://github.com/844196/Renge"
-    url "https://github.com/844196/Renge/archive/v0.4.tar.gz"
-    sha256 "b59056d34853b335796051e59b036d3dd8e3664409601d01eaeec7d42d25e370"
+    url "https://github.com/844196/Renge/archive/v0.6.tar.gz"
+    sha256 "17460aec341f140d4b50614d8d42c769f3f3675f9a504c85c6cf430752b81cf1"
     head "https://github.com/844196/Renge.git"
-    version "0.4"
+    version "0.6"
 
     def install
         system "make", "install", "PREFIX=#{prefix}"
