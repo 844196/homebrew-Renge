75a01f94ead7c1299fe05e1516ba4a0c1d38244f
Masaya Takeda <844196@gmail.com>
2015-01-29 16:45:18 +0900

    yasuna ver 0.3.6

--- yasuna.rb
+++ yasuna.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Yasuna < Formula
     homepage "https://github.com/sasairc/yasuna"
-    url "http://ssiserver.moe.hm/debian/yasuna_0.3.4-1.tar.gz"
-    sha256 "25df667f121a03edab3e8067cd2f1c1b7eb9cc6d3b29c0283ae2add410b30411"
-    version "0.3.4"
+    url "https://github.com/sasairc/yasuna/archive/v0.3.6.tar.gz"
+    sha256 "9e78255db0fcbdddc08a0b74e5b53d3db9265f4273aa10a6f628097947d4b567"
+    version "0.3.6"
 
     def install
         system "make", "PREFIX=#{prefix}"
