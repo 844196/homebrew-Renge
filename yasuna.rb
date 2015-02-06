446ca2bb591665ad2eed006ba3cf0d0d2177558c
Masaya Takeda <844196@gmail.com>
2015-02-07 02:45:20 +0900

    yasuna ver 0.3.8

--- yasuna.rb
+++ yasuna.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Yasuna < Formula
     homepage "https://github.com/sasairc/yasuna"
-    url "https://github.com/sasairc/yasuna/archive/v0.3.7.tar.gz"
-    sha256 "591474dcaf4ffdf6ed535b586814521c87df874ce1520a297cc151329d4a1442"
-    version "0.3.7"
+    url "https://github.com/sasairc/yasuna/archive/v0.3.8.tar.gz"
+    sha256 "381563832e751041e6eb06b85e165c86c40d3e5d37d8d85e5e8eb6c34d78f099"
+    version "0.3.8"
 
     def install
         system "make", "PREFIX=#{prefix}"
