09ac1995a95c8ae0ab875755ccbfbff0b889bbb2
Masaya Takeda <844196@gmail.com>
2015-03-12 23:18:02 +0900

    fix???

--- clangsay.rb
+++ clangsay.rb
@@ -6,20 +6,23 @@ class Clangsay < Formula
     sha256 "35bd8c0eb61be8b26482b682ddaa4fc51f56bf644222a996bccd74613b1330a3"
     version "0.0.5"
 
+    option "zsh-completion", "Install zsh completion"
+
     depends_on "cowsay"
     depends_on "pkg-config"
     depends_on "glib"
-
-    option "zsh-completion", "Install zsh completion"
+    if build.include?('zsh-completion')
+        depends_on "zsh"
+    end
 
     def install
-        system "make", "PREFIX=#{prefix}", "COWPATH=#{HOMEBREW_PREFIX}/share/cows/"
+        system "make", "PREFIX=#{prefix}", "COWPATH=#{HOMEBREW_PREFIX}/share/cows"
         system "make", "install", "PREFIX=#{prefix}"
 
-        if build.include?('zsh-completion') && ENV.include?('FPATH')
-            ENV['FPATH'].split(':').find do |path|
-                system "install", "-pm 644", "_clangsay", "#{path}" if File.directory? path
-            end
+        if build.include?('zsh-completion')
+            depends_on "zsh"
+            zsh_functions_d = share + 'zsh/functions'
+            zsh_functions_d.install "_clangsay"
         end
     end
 end
