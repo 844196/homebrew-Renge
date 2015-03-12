94bcc06d224fb829161f46745683f0e3d77f7d75
Masaya Takeda <844196@gmail.com>
2015-03-12 22:24:23 +0900

    fix?

--- clangsay.rb
+++ clangsay.rb
@@ -2,14 +2,21 @@ require "formula"
 
 class Clangsay < Formula
     homepage "https://github.com/sasairc/clangsay"
-    url "https://github.com/sasairc/clangsay/archive/v0.0.4.tar.gz"
-    sha256 "316883b32c4db90d97a0a37b3e8a2006ae4eb24ea71f6a054e0ea7b09c105cc8"
-    version "0.0.4"
+    url "https://github.com/sasairc/clangsay/archive/v0.0.5.tar.gz"
+    sha256 "35bd8c0eb61be8b26482b682ddaa4fc51f56bf644222a996bccd74613b1330a3"
+    version "0.0.5"
 
     depends_on "cowsay"
+    depends_on "pkg-config"
+    depends_on "glib"
 
     def install
-        system "make", "PREFIX=#{prefix}"
+        system "make", "PREFIX=#{prefix}", "COWPATH=#{HOMEBREW_PREFIX}/share/cows/"
+        if ARGV.include?('--zsh-completion') && ENV.include?('FPATH')
+            ENV['FPATH'].split(':').find do |path|
+                system "install", "-pm 644", "_clangsay", "#{path}" if File.directory? path
+            end
+        end
         system "make", "install", "PREFIX=#{prefix}"
     end
 end
