2f834dee25171ae22f18ed1384d640aea7656279
Masaya Takeda <844196@gmail.com>
2016-09-18 20:38:15 +0900

    clangsay v1.6.2

--- clangsay.rb
+++ clangsay.rb
@@ -1,32 +1,18 @@
 require "formula"
 
 class Clangsay < Formula
-    homepage "https://github.com/sasairc/clangsay"
-    url "https://api.github.com/repos/sasairc/clangsay/tarball/v1.4.0"
-    sha256 "36c209781ca97baf71f3de9a85a893d07b252c403862ba1f15ab3a48f4f2dd27"
-    version "1.4.0"
+  desc "The classic cowsay program, written in C."
+  homepage "https://github.com/sasairc/clangsay"
+  url "https://github.com/sasairc/clangsay/archive/v1.6.2.tar.gz"
+  sha256 "67cf072ed2184f4a2d42e0656938e25b94b53ed5fae371ccc4175f2e76475ea1"
+  version "1.6.2"
 
-    option "zsh-completion", "Install zsh completion"
-    option "without-cows", "Without cowfile"
+  depends_on "cowsay"
+  depends_on "pkg-config"
+  depends_on "glib"
 
-    depends_on "cowsay"
-    depends_on "pkg-config"
-    depends_on "glib"
-
-    def install
-        cowpath = "#{HOMEBREW_PREFIX}" + '/share/cows'
-
-        system "make", "PREFIX=#{prefix}", "COWPATH=#{cowpath}"
-        system "make", "install", "PREFIX=#{prefix}"
-
-        if !(build.include?('without-cows'))
-            rm "cows/default.cow"
-            share.install Dir["cows"]
-        end
-
-        if build.include?('zsh-completion')
-            prefix.install "_clangsay"
-            zsh_completion.install "#{prefix}/_clangsay"
-        end
-    end
+  def install
+    system "make", "install", "PREFIX=#{prefix}", "COWPATH=#{HOMEBREW_PREFIX}/share/cows", "MANDIR=#{man6}"
+    zsh_completion.install "#{prefix}/share/clangsay/compdef/_clangsay.zsh"
+  end
 end
